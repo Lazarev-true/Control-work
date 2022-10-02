cd594664fc15c78b529acd8240ddfa20cd49f540
Konstantin <klik@mail.ru>
2022-10-02 18:14:57 +0300

    Создание и вывод нового массива

--- Task/Program.cs
+++ Task/Program.cs
@@ -24,3 +24,43 @@ for (int i = 0; i < count; i++)
 
 string strOld = string.Join(", ", arrayOld);
 Console.Write($"[{strOld}] -> ");
+
+Random rand = new Random();
+
+if(count > 3)
+    N = rand.Next(4);
+else
+    N = rand.Next(count + 1);
+
+string[] arrayNew = new string[N];
+int[] number = new int[N];
+
+    if(N == 0)
+    Console.Write($"[]");
+else
+{
+    number[0] = rand.Next(0, 4);
+
+    for (int i = 1; i < N;)
+{
+    int tmp = rand.Next(0, N);
+    int j;
+    for (j = 0; j < i; j++)
+    {
+        if (tmp == number[j])
+        break;
+    }
+    if (j == i)
+    { 
+        number[i] = tmp;
+        i++; 
+    }
+}
+
+    for (int i = 0; i < N; i++)
+    {
+        arrayNew[i] = arrayOld[number[i]]; 
+    }
+    string strNew = string.Join(", ", arrayNew);
+    Console.Write($"[{strNew}]");
+}
